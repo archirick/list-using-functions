403d344d257aa4e44001fb196fdf0f8d0f4d9135
archirick <48961843+archirick@users.noreply.github.com>
2019-04-06 19:51:48 +0530

    Add files via upload

---
+++ list using function.py
@@ -0,0 +1,12 @@
+def change_list(list1):
+    list1.append(20)
+    list1.append(30)
+print("list inside function = ",list10)
+
+#defining the list
+list10=[10,20,30,40,50]
+list11=[11,21,31,41,51]
+
+#calling the function
+change_list(list11)
+print("The outside list = ",list11)
